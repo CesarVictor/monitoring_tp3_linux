5cbfc2ba6d0afacd7844459b36e3e38db3c68574
CesarVictor <cesarvictor@hotmail.fr>
2024-02-01 15:54:17 +0100

    api

--- monit-api.py
+++ monit-api.py
@@ -28,4 +28,4 @@ def get_report(report_id=None):
         abort(404)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
